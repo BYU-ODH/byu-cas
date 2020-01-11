1711c876b13eeeebf91ca914546c3aef3e9e2200
Tory S. Anderson <torys.anderson@gmail.com>
2020-01-10 21:11:49 -0700

    Version 1: upgrades

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject byu-odh/byu-cas "0.1.5"
+(defproject byu-odh/byu-cas "1"
   :description "BYU CAS authentication layer for Clojure"
   :url "https://github.com/BYU-ODH/byu-cas"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.jasig.cas.client/cas-client-core "3.2.1"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [ring/ring-defaults "0.2.0"]
-                 [ring-middleware-format "0.7.0"]
-                 [ring/ring-defaults "0.2.0"]
-                 [metosin/ring-http-response "0.6.5"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.jasig.cas.client/cas-client-core "3.6.1"]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring-middleware-format "0.7.4"]
+                 [ring/ring-defaults "0.3.2"]
+                 [metosin/ring-http-response "0.9.1"]])
