1707658f951ed26a90bc15fe31336e77981cded2
Daniel Bell <dchristianbell@gmail.com>
2020-06-19 08:09:11 -0600

    if query params include a ticket param, validates, starts a session, and redirects to same uri w/o ticket query param

--- src/byu_cas/core.clj
+++ src/byu_cas/core.clj
@@ -3,11 +3,9 @@
             [ring.middleware.params :refer [wrap-params]]
             [ring.util.response :refer [redirect]]
             [clojure.pprint :as pprint]
-            [clojure.string :refer [join]])
+            [clojure.string :refer [join] :as s])
   (:import (org.jasig.cas.client.validation Cas10TicketValidator
                                             TicketValidationException)))
-
-
 
 (def BYU-CAS-server "https://cas.byu.edu/cas")
 
@@ -50,69 +48,52 @@
          {:status 403}
          (redirect (str cas-server "/login?service=" service)))))))
 
-(defn session-assertion [res assertion]
-  (assoc-in res [:session const-cas-assertion] assertion))
+(defn adds-assertion-to-response [resp assertion]
+  (assoc-in resp [:session const-cas-assertion] assertion))
 
-(defn request-assertion [req assertion]
+#_(defn adds-assertion-to-request [req assertion]
   (update-in req [:query-params] assoc const-cas-assertion assertion))
 
 (defn ticket [r] (or (get-in r [:query-params artifact-parameter-name])
                      (get-in r [:query-params (keyword artifact-parameter-name)])))
 
-(defn ticket-validation-filter-maker []
-  (fn [handler service]
-    (let [ticket-validator (validator-maker)]
-      (fn [request]
-        (if-let [t (ticket request)]
-          (try
-            (let [assertion (validate ticket-validator t service)]
-              (session-assertion (handler (request-assertion request assertion)) assertion))
-            (catch TicketValidationException e
-              (log/error "Ticket validation exception " e)
-              {:status 403}))
-          (handler request))))))
-
-(def ticket-validation-filter
-  "Actually validates the ticket, updates the request and response with the result."
-  (ticket-validation-filter-maker))
+(defn ticket-validation-filter [handler service]
+  (let [ticket-validator (validator-maker)]
+    (fn [{:keys [query-params uri] :as request}]
+      (if-let [t (ticket request)]
+        (try
+          (let [assertion (validate ticket-validator t service)]
+            (-> (redirect (construct-url uri (dissoc query-params "ticket")))
+                (adds-assertion-to-response assertion)))
+          (catch TicketValidationException e
+            (println "failed validation")
+            (log/error "Ticket validation exception " e)
+            {:status 403}))
+        (handler request)))))
 
 (defn user-principal-filter
   "Takes username and cas-info from request, and moves them into :username and :cas-info keys in the top level of the request map (rather than being buried in :query-params or :session)."
   [handler]
   (fn [request]
-    (if-let [assertion (or (get-in request [:query-params const-cas-assertion])
-                           (get-in request [:query-params (keyword const-cas-assertion)])
-                           (get-in request [:session const-cas-assertion])
-                           (get-in request [:session (keyword const-cas-assertion)]))]
-      (handler (-> request
-                   (assoc :username (.getName (.getPrincipal assertion)))
+    (if-let [assertion (or
+                        (get-in request [:query-params const-cas-assertion])
+                        (get-in request [:query-params (keyword const-cas-assertion)])
+                        (get-in request [:session const-cas-assertion])
+                        (get-in request [:session (keyword const-cas-assertion)]))]
+      (do
+        (handler (-> request
+                            (assoc :username (.getName (.getPrincipal assertion)))
                                         ;(assoc :cas-info (.getAttributes assertion))
-                   (assoc :cas-info (.getAttributes (.getPrincipal assertion)))
-                   ))
+                            (assoc :cas-info (.getAttributes (.getPrincipal assertion))))))
       (handler request))))
-
-(defn does-to-request [handler f]
-  (fn [request]
-    (handler (f request))))
-
-(defn prints-req [r]
- (do (pprint/pprint r) r))
 
 (defn construct-url [uri query-params]
-  (let [res (str uri
-                 (when-not (empty? query-params)
-                   (->> query-params
-                        (map (fn [[k v]] (str (name k) "=" (str v))))
-                        (join \&)
-                        (str \?))))]
-    (println "construct-uri:" res)
-    res))
-
-(defn removes-url-token [handler]
-  (fn [{:keys [query-params uri] :as request}]
-    (if (query-params "ticket")
-      (redirect (construct-url uri (dissoc query-params "ticket")))
-      (handler request))))
+  (str uri
+       (when-not (empty? query-params)
+         (->> query-params
+              (map (fn [[k v]] (str (name k) "=" (str v))))
+              (join \&)
+              (str \?)))))
 
 (defn cas
   "Middleware that requires the user to authenticate with a CAS server.
@@ -137,10 +118,8 @@
         (authentication-filter service (:no-redirect? options) (:server options))
         (ticket-validation-filter service)
         #_(removes-url-token)
-        #_(does-to-request prints-req)
         (wrap-params))
       handler)))
-
 
 
 (defn wrap-remove-cas-code [handler]
--- test/byu_cas/core_test.clj
+++ test/byu_cas/core_test.clj
@@ -1,11 +1,21 @@
  (ns byu-cas.core-test
      (:require [byu-cas.core :refer :all]
-               [luminus.http-server :as http]))
+                 [luminus.http-server :as http]
+                 [ring.middleware.session :refer [wrap-session]]))
 
 (defn http-handler [request]
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body (:remote-addr request)})
+
+(defn generate-app []
+  (-> http-handler
+      (wrap-cas #_"https://my.byu.edu/uPortal/Login"
+                #_"humanities.byu.edu/wp-login.php"
+                #_"https://forms.byu.edu"
+                "http://localhost:3000"
+                #_"kmansadventures.proboards.com")
+      (wrap-session))) 
 
 (def easy-map
   {:handler http-handler
@@ -13,22 +23,13 @@
 
 (def running-server (atom nil))
 
-(defn test-with-middleware [mw-fn]
-  (reset! running-server  (http/start
-                   {:port 3000
-                    :handler (mw-fn http-handler)})))
+(defn start []
+  (reset! running-server (http/start
+                          {:port 3000
+                           :handler (generate-app)})))
 
 (defn stop []
   (if @running-server
     (http/stop @running-server)))
 
 ;https://my.byu.edu/uPortal/Login
-
-(defn start []
-  (test-with-middleware #(wrap-cas % #_"https://my.byu.edu/uPortal/Login"
-                                   
-                                   "humanities.byu.edu/wp-login.php"
-                                   #_"https://forms.byu.edu"
-                                   #_"localhost:3000"
-                                   #_"kmansadventures.proboards.com"
-                                   )))
